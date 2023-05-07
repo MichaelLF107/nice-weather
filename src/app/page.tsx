ac4c2504e66b897bdce3a97c8aa25f09bec85aa7
Michael Lanius Friedrich <michaelfriedrich253@gmail.com>
2023-05-07 00:58:09 -0300

    Created home page 🎉

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,109 @@
-import Image from 'next/image'
+"use client"
+
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import { getWeatherByCoords, getCoordsByCity } from '@/service/weatherService'
+import { TextField, Button, CircularProgress } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import WeatherCard from '@/components/WeatherCard/WeatherCard'
+
+type Coordinates = {
+  accuracy: number | null
+  altitude: number | null
+  altitudeAccuracy: number | null
+  heading: number | null
+  latitude: number | null
+  longitude: number | null
+  speed: number | null
+}
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
+function formatDate(date: string) {
+  const dateObj = new Date(date)
+  const day = dateObj.getDate()
+  const month = dateObj.getMonth() + 1
+  const year = dateObj.getFullYear()
+  const hours = dateObj.getHours()
+  const minutes = dateObj.getMinutes()
+  const ampm = hours >= 12 ? 'pm' : 'am'
+  const formattedDay = day < 10 ? `0${day}` : day
+  const formattedMonth = month < 10 ? `0${month}` : month
+  const formattedHours = hours % 12 ? hours % 12 : 12
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes
+  return `${formattedDay}/${formattedMonth}/${year} - ${formattedHours}:${formattedMinutes}${ampm}`
+}
 
 export default function Home() {
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({} as any)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const weatherData = await getWeatherByCoords(position.coords.latitude, position.coords.longitude)
+        setWeather(weatherData)
+        setCity(weatherData.city.name)
+        setLoading(false)
+      })
+    }
+  }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <main className={styles.main}>
+        <div className={styles.citySelector}>
+          <TextField
+            id="city"
+            label="City"
+            variant="outlined"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            onClick={async () => {
+              setLoading(true)
+              let coords = await getCoordsByCity(city)
+              coords = coords.results[0].locations[0].latLng
+              const weatherData = await getWeatherByCoords(coords.lat, coords.lng)
+              setWeather(weatherData)
+              setLoading(false)
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Get Weather
+          </Button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {!loading ? (
+          <div className={styles.list}>
+            {weather.list.map((item: any) => (
+              <div key={item.dt} className={styles.item}>
+                <WeatherCard
+                  main={item.main}
+                  name={weather.city.name}
+                  rain={item.rain}
+                  weather={item.weather}
+                  wind={item.wind}
+                />
+                <span className={styles.date}>
+                  <CalendarMonthIcon fontSize="inherit" />
+                  {formatDate(item.dt_txt)}
+                </span>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className={styles.loading}>
+            <CircularProgress />
+          </div>
+        )}
+      </main>
+    </ThemeProvider>
   )
 }
